9ed4e16991dfbf27233b814b273239618b31f347
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-09 20:44:52 -0500

    se agrego los botones de la calculadora y sus estilos css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
-
+import logoimg from '../src/assets/imagenes/stock-photo-cup.jpeg';
+import Boton from './components/Boton';
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img
+          src={logoimg}
+          className='logo'
+          alt='logo'
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
